02b1553a7135728ab4b8c7fe46301f7fde461a59
Thorji <ledo26611@gmail.com>
2022-06-19 13:42:18 -0300

    desafio1 feito

--- DesafioTypeScript/desafio1.ts
+++ DesafioTypeScript/desafio1.ts
@@ -1,8 +1,31 @@
 /*
-// Como podemos rodar isso em um arquivo .ts sem causar erros? 
+Como podemos rodar isso em um arquivo .ts sem causar erros? 
 
 let employee = {};
 employee.code = 10;
 employee.name = "John";
 */
 
+//** 1 */
+interface IEmployee{
+    code: number;
+    name: string;
+}
+
+const RandomEmployee : IEmployee = {
+    code: 10,
+    name: "John"
+}
+
+//** 2 */
+
+const employee = { 
+    code: 10, 
+    name: "John" 
+};
+
+//** 3 */
+const employee2: {code: number, name: string} = {
+    code: 10,
+    name: 'john'
+}
